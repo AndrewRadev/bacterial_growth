d26ec8a96d7491b81ce298486bee331054b55392
Andrew Radev <andrey.radev@gmail.com>
2025-06-22 18:22:42 +0200

    Fix model export button

--- app/model/lib/model_export.py
+++ app/model/lib/model_export.py
@@ -33,6 +33,8 @@ def export_model_csv(db_session, study):
         measurement_context = modeling_result.measurementContext
         subject = measurement_context.get_subject(db_session)
 
+        params = modeling_result.params
+
         writer.writerow({
             'bioreplicate': measurement_context.bioreplicate.name,
             'compartment':  measurement_context.compartment.name,
@@ -40,18 +42,18 @@ def export_model_csv(db_session, study):
             'subject_name': subject.name,
             'model_type':   modeling_result.model_name,
             # Inputs:
-            'input_pointCount': modeling_result.inputs.get('pointCount', None),
-            'input_endTime':    modeling_result.inputs.get('endTime', None),
+            'input_pointCount': params['inputs'].get('pointCount', None),
+            'input_endTime':    params['inputs'].get('endTime', None),
             # Coefficients:
-            'y0':    modeling_result.coefficients.get('y0',    None),
-            'mumax': modeling_result.coefficients.get('mumax', None),
-            'lag':   modeling_result.coefficients.get('lag',   None),
-            'y0_lm': modeling_result.coefficients.get('y0_lm', None),
-            'K':     modeling_result.coefficients.get('K',     None),
-            'h0':    modeling_result.coefficients.get('h0',    None),
+            'y0':    params['coefficients'].get('y0',    None),
+            'mumax': params['coefficients'].get('mumax', None),
+            'lag':   params['coefficients'].get('lag',   None),
+            'y0_lm': params['coefficients'].get('y0_lm', None),
+            'K':     params['coefficients'].get('K',     None),
+            'h0':    params['coefficients'].get('h0',    None),
             # Fit:
-            'r2':  modeling_result.fit.get('r2',  None),
-            'rss': modeling_result.fit.get('rss', None),
+            'r2':  params['fit'].get('r2',  None),
+            'rss': params['fit'].get('rss', None),
         })
 
     return buf.getvalue().encode('utf-8')
