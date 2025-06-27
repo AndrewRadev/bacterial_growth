98deb9c24a94a5764356398cb18cb3b8e8551921
Andrew Radev <andrey.radev@gmail.com>
2025-06-27 16:07:25 +0200

    Take names from KEGG

--- scripts/external/chebi/download_dump.py
+++ scripts/external/chebi/download_dump.py
@@ -1,4 +1,5 @@
 import csv
+import re
 from pathlib import Path
 
 import pandas as pd
@@ -11,27 +12,34 @@ from app.model.lib.util import download_file, gunzip
 # -> Print missing chebi ids
 # -> Print names in the database that are not in the dumps
 
-base_url = 'https://ftp.ebi.ac.uk/pub/databases/chebi/Flat_file_tab_delimited'
-base_dir = Path('var/external_data/')
+chebi_url = 'https://ftp.ebi.ac.uk/pub/databases/chebi/Flat_file_tab_delimited'
+mco_url   = 'https://raw.githubusercontent.com/microbial-conditions-ontology/microbial-conditions-ontology/master/mco.owl'
+base_dir  = Path('var/external_data/chebi/')
 
-# names_gz_path      = base_dir / 'names_3star.tsv.gz'
-# names_path         = base_dir / 'names_3star.tsv'
-# chemical_data_path = base_dir / 'chemical_data_3star.tsv'
+base_dir.mkdir(exist_ok=True)
 
 names_gz_path      = base_dir / 'names.tsv.gz'
 names_path         = base_dir / 'names.tsv'
 chemical_data_path = base_dir / 'chemical_data.tsv'
+mco_owl_path       = base_dir / 'mco.owl'
 
 output_path = base_dir / 'combined_dump.csv'
 
-with print_with_time("Downloading raw files"):
-    download_file(f"{base_url}/names.tsv.gz",      names_gz_path)
-    download_file(f"{base_url}/chemical_data.tsv", chemical_data_path)
-
-    # download_file(f"{base_url}/names_3star.tsv.gz",      names_gz_path)
-    # download_file(f"{base_url}/chemical_data_3star.tsv", chemical_data_path)
+with print_with_time("Downloading raw data files"):
+    download_file(f"{chebi_url}/names.tsv.gz",      names_gz_path)
+    download_file(f"{chebi_url}/chemical_data.tsv", chemical_data_path)
+    download_file(mco_url, mco_owl_path)
 
     gunzip(names_gz_path, names_path)
+
+target_chebi_ids = None
+
+with print_with_time("Processing MCO owl file"):
+    with open(mco_owl_path, 'r') as owlfile:
+        owl_text = owlfile.read()
+
+    chebi_pattern = re.compile(r'//purl.obolibrary.org/obo/CHEBI_(\d+)')
+    target_chebi_ids = {int(chebi_id) for chebi_id in chebi_pattern.findall(owl_text)}
 
 data = {}
 
@@ -42,15 +50,16 @@ with print_with_time("Parsing data into memory"):
         for row in reader:
             if row['LANGUAGE'] != 'en':
                 continue
-            if row['TYPE'] != 'IUPAC NAME':
+            if row['SOURCE'] != 'KEGG COMPOUND':
                 continue
 
-            # TODO (2025-06-26) Collect all IUPAC names, use unique "name" index instead?
-
             chebi_id = int(row['COMPOUND_ID'])
+            if chebi_id not in target_chebi_ids:
+                continue
+
             if chebi_id not in data:
                 data[chebi_id] = {
-                    'name':        row['NAME'],
+                    'name':        row['NAME'].lower(),
                     'averageMass': None,
                 }
 
@@ -72,11 +81,10 @@ with print_with_time("Creating combined dump"):
         writer = csv.DictWriter(
             f,
             fieldnames=['chebiId', 'name', 'averageMass'],
+            dialect='unix',
+            quoting=csv.QUOTE_MINIMAL,
         )
         writer.writeheader()
 
         for chebi_id in sorted(data.keys()):
-            writer.writerow({
-                'chebiId': chebi_id,
-                **data[chebi_id]
-            })
+            writer.writerow({'chebiId': chebi_id, **data[chebi_id]})
