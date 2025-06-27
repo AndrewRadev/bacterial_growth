1ad0f21a5a1aeec4af1d96a8d317652ee9447023
Andrew Radev <andrey.radev@gmail.com>
2025-06-27 11:53:05 +0200

    Metabolite download notes

--- scripts/external/chebi/download_dump.py
+++ scripts/external/chebi/download_dump.py
@@ -7,8 +7,16 @@ from long_task_printer import print_with_time
 
 from app.model.lib.util import download_file, gunzip
 
+# TODO (2025-06-26) use Mco.owl to pick chebi ids, limit results to those.
+# -> Print missing chebi ids
+# -> Print names in the database that are not in the dumps
+
 base_url = 'https://ftp.ebi.ac.uk/pub/databases/chebi/Flat_file_tab_delimited'
 base_dir = Path('var/external_data/')
+
+# names_gz_path      = base_dir / 'names_3star.tsv.gz'
+# names_path         = base_dir / 'names_3star.tsv'
+# chemical_data_path = base_dir / 'chemical_data_3star.tsv'
 
 names_gz_path      = base_dir / 'names.tsv.gz'
 names_path         = base_dir / 'names.tsv'
@@ -19,6 +27,10 @@ output_path = base_dir / 'combined_dump.csv'
 with print_with_time("Downloading raw files"):
     download_file(f"{base_url}/names.tsv.gz",      names_gz_path)
     download_file(f"{base_url}/chemical_data.tsv", chemical_data_path)
+
+    # download_file(f"{base_url}/names_3star.tsv.gz",      names_gz_path)
+    # download_file(f"{base_url}/chemical_data_3star.tsv", chemical_data_path)
+
     gunzip(names_gz_path, names_path)
 
 data = {}
@@ -40,8 +52,6 @@ with print_with_time("Parsing data into memory"):
                 data[chebi_id] = {
                     'name':        row['NAME'],
                     'averageMass': None,
-                    'description': None,
-                    'createdOn':   None,
                 }
 
     with open(chemical_data_path) as f:
