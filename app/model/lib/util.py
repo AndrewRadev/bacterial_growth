a846971ba34931082b4113cd63d23e9660d68999
Andrew Radev <andrey.radev@gmail.com>
2025-06-30 11:18:41 +0200

    READMEs and minor code improvements

--- app/model/lib/util.py
+++ app/model/lib/util.py
@@ -7,11 +7,12 @@ import tarfile
 import shutil
 from io import BytesIO
 from pathlib import Path
+from typing import Optional, Iterable
 
 import requests
 
 
-def is_non_negative_float(string, *, isnan_check):
+def is_non_negative_float(string: str, *, isnan_check: bool):
     try:
         value = float(string)
         if isnan_check:
@@ -22,7 +23,7 @@ def is_non_negative_float(string, *, isnan_check):
         return False
 
 
-def trim_lines(string):
+def trim_lines(string: str):
     return "\n".join(
         line.strip()
         for line in string.splitlines()
@@ -41,19 +42,19 @@ def createzip(csv_data: list[tuple[str, bytes]]):
     return buf
 
 
-def group_by_unique_name(collection):
+def group_by_unique_name(collection: Iterable):
     return {
         name: _one_or_error(name, group)
         for (name, group) in itertools.groupby(collection, lambda c: c.name)
     }
 
 
-def humanize_camelcased_string(string):
+def humanize_camelcased_string(string: str):
     return re.sub(r'([a-z])([A-Z])', r'\1 \2', string)
 
 
 # Adapted from: https://stackoverflow.com/a/16696317
-def download_file(url, filename):
+def download_file(url: str, filename: str):
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
 
@@ -62,7 +63,7 @@ def download_file(url, filename):
                 f.write(chunk)
 
 
-def gunzip(path, extracted_path=None):
+def gunzip(path: Path, extracted_path: Optional[Path] = None):
     gz_path = Path(path)
     if gz_path.suffix != '.gz':
         raise ValueError(f"Path doesn't end in .gz: {path}")
@@ -74,7 +75,11 @@ def gunzip(path, extracted_path=None):
         with open(extracted_path, 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
 
-def untar(path, target_dir, file_list):
+
+def untar(path: Path, target_dir: Path, file_list: list[str]):
+    """
+    Extract individual files from a tar file into the given `target_dir`.
+    """
     tar_path = Path(path)
     if '.tar' not in tar_path.suffixes:
         raise ValueError(f"Path doesn't include .tar suffix: {path}")
@@ -84,6 +89,7 @@ def untar(path, target_dir, file_list):
     with tarfile.open(tar_path, 'r') as tar_f:
         for filename in file_list:
             tar_f.extract(filename, path=target_dir)
+
 
 def _one_or_error(key, iterator):
     value = next(iterator)
--- scripts/external/chebi/download_dump.py
+++ scripts/external/chebi/download_dump.py
@@ -2,32 +2,27 @@ import csv
 import re
 import itertools
 from pathlib import Path
-from datetime import date
-from urllib.error import URLError
 
-import pandas as pd
 from bioservices import ChEBI
 from long_task_printer import print_with_time, LongTask
 
-from app.model.lib.util import download_file, gunzip
+from app.model.lib.util import download_file
 
 base_dir = Path('var/external_data/chebi/')
 base_dir.mkdir(exist_ok=True)
 
-mco_url = 'https://raw.githubusercontent.com/microbial-conditions-ontology/microbial-conditions-ontology/master/mco.owl'
-
-mco_owl_path = base_dir / 'mco.owl'
 output_path  = base_dir / 'data_dump.csv'
 
 target_chebi_ids = None
 
 with print_with_time("Downloading and processing MCO owl file"):
+    mco_url = 'https://raw.githubusercontent.com/microbial-conditions-ontology/microbial-conditions-ontology/master/mco.owl'
+    mco_owl_path = base_dir / 'mco.owl'
     download_file(mco_url, mco_owl_path)
 
-    with open(mco_owl_path, 'r') as owlfile:
-        owl_text = owlfile.read()
-
+    owl_text      = mco_owl_path.read_text()
     chebi_pattern = re.compile(r'//purl.obolibrary.org/obo/CHEBI_(\d+)')
+
     target_chebi_ids = {int(chebi_id) for chebi_id in chebi_pattern.findall(owl_text)}
 
 data = {}
@@ -40,14 +35,7 @@ with print_with_time("Fetching data from ChEBI API"):
         with long_task.measure() as progress:
             print(progress)
 
-            for retry in range(3):
-                try:
-                    entities = chebi_api.getCompleteEntityByList(chebi_ids)
-                    break
-                except URLError as e:
-                    print(f"Connection error, retry {retry}: {e}")
-                    time.sleep(1)
-                    continue
+            entities = chebi_api.getCompleteEntityByList(chebi_ids)
 
             for entity in entities:
                 chebi_id = int(entity.chebiId.split(':')[-1])
