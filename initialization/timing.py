c5f503e2a2f3e75cc0d7a2cc9bcdac02f9777932
Andrew Radev <andrey.radev@gmail.com>
2025-07-03 13:42:14 +0200

    Fixes for database stability and time measuring

--- initialization/timing.py
+++ initialization/timing.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 from flask import g, current_app, request
@@ -9,27 +10,28 @@ def init_timing(app):
     app.before_request(start_request_timing)
     app.after_request(log_request_timing)
 
-    sql_event.listens_for(Engine, "before_cursor_execute", start_db_timing)
-    sql_event.listens_for(Engine, "after_cursor_execute", log_db_timing)
+    sql_event.listens_for(Engine, "before_cursor_execute")(start_db_timing)
+    sql_event.listens_for(Engine, "after_cursor_execute")(log_db_timing)
 
     return app
 
 
 def start_request_timing():
     g.start_time = time.monotonic_ns()
-    g.sql_time_ns = 0.0
 
 
 def log_request_timing(response):
-    if not request.full_path.startswith('/static'):
-        duration_ns = time.monotonic_ns() - g.start_time
-        duration_ms = round(duration_ns / 1_000_000, 2)
+    if request.full_path.startswith('/static'):
+        return response
 
-        sql_duration_ms = round(g.sql_time_ns / 1_000_000, 2)
+    duration_ns = time.monotonic_ns() - g.start_time
+    duration_ms = round(duration_ns / 1_000_000, 2)
 
-        logger = current_app.logger.getChild('timing')
-        logger.info(f"[{duration_ms}ms] Full request total")
-        logger.info(f"[{sql_duration_ms}ms] Full request SQL")
+    sql_duration_ms = round(g.sql_time_ns / 1_000_000, 2)
+
+    logger = current_app.logger.getChild('timing')
+    logger.info(f"[{duration_ms}ms] Full request total")
+    logger.info(f"[{sql_duration_ms}ms] Full request SQL")
 
     return response
 
@@ -38,6 +40,9 @@ def log_request_timing(response):
 # https://docs.sqlalchemy.org/en/20/faq/performance.html#how-can-i-profile-a-sqlalchemy-powered-application
 #
 def start_db_timing(conn, cursor, statement, parameters, context, executemany):
+    if 'sql_time_ns' not in g:
+        g.sql_time_ns = 0.0
+
     start_time = time.monotonic_ns()
     conn.info.setdefault("start_time", []).append(start_time)
 
@@ -48,5 +53,6 @@ def log_db_timing(conn, cursor, statement, parameters, context, executemany):
 
     g.sql_time_ns += duration_ns
 
-    logger = current_app.logger.getChild('timing')
-    logger.info(f"[{duration_ms}ms] Query: {statement}")
+    if os.getenv('TIME'):
+        logger = current_app.logger.getChild('timing')
+        logger.info(f"[{duration_ms}ms] Query: {statement}")
