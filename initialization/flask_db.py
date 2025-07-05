b2f08ae202f8fedf28a6152cecdd3d27a5d0b128
Andrew Radev <andrey.radev@gmail.com>
2025-07-05 11:37:48 +0200

    Minor rename and comment note

--- initialization/flask_db.py
+++ initialization/flask_db.py
@@ -1,11 +1,11 @@
 import simplejson as json
 
-from db import get_config_uri, FLASK_DB, SQLALCHEMY_ENGINE_OPTIONS
+from db import get_config_uri, FLASK_DB, APP_SQLALCHEMY_ENGINE_OPTIONS
 
 
 def init_flask_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = get_config_uri()
-    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = SQLALCHEMY_ENGINE_OPTIONS
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = APP_SQLALCHEMY_ENGINE_OPTIONS
 
     FLASK_DB.init_app(app)
 
