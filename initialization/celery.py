0153bca4e4d5ff8d3956196c9e4bb6c8eada46c2
Andrew Radev <andrey.radev@gmail.com>
2025-07-12 17:19:04 +0200

    Wait for mysql to start, connect to redis

--- initialization/celery.py
+++ initialization/celery.py
@@ -1,20 +1,31 @@
+import os
+
 from celery import Celery, Task
 
 
 def init_celery(app):
     """
-    The redis config hardcodes port 6380, which is the same one used in the
-    docker image.
+    The redis config defaults to port 6380 of localhost, which is the same one
+    used in the "services" docker image.
+
+    This can be overridden by two environment variables:
+    - REDIS_HOST
+    - REDIS_PORT
     """
     class AppTask(Task):
         def __call__(self, *args: object, **kwargs: object) -> object:
             with app.app_context():
                 return self.run(*args, **kwargs)
 
+    redis_host = os.getenv('REDIS_HOST', 'localhost')
+    redis_port = os.getenv('REDIS_PORT', '6380')
+
+    redis_url = f"redis://{redis_host}:{redis_port}/0"
+
     celery_app = Celery(app.name, task_cls=AppTask)
     celery_app.config_from_object({
-        'broker_url':         "redis://localhost:6380/0",
-        'result_backend':     "redis://localhost:6380/0",
+        'broker_url':         redis_url,
+        'result_backend':     redis_url,
         'task_ignore_result': True,
     })
     celery_app.set_default()
