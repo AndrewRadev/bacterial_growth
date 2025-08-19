2da8f9f292e889463b5562ef520d23d871ec178a
Andrew Radev <andrey.radev@gmail.com>
2025-08-19 11:35:59 +0200

    Record changelog entries, clean up old files

--- scripts/export_studies.py
+++ scripts/export_studies.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from datetime import datetime, UTC
 
 import simplejson as json
 import sqlalchemy as sql
@@ -14,11 +15,19 @@ with get_session() as db_session:
         .order_by(Study.publicId)
     ).all()
 
+    timestamp = datetime.now(UTC)
+
     for study in studies:
         base_dir = Path(f"static/export/{study.publicId}")
         base_dir.mkdir(parents=True, exist_ok=True)
 
         submission = study.find_last_submission(db_session)
+
+        # Clean up previous files:
+        for file in base_dir.glob('*.csv'):
+            file.unlink()
+        for file in base_dir.glob('*.json'):
+            file.unlink()
 
         # Export study design:
         with open(base_dir / 'study_design.json', 'w') as f:
@@ -28,3 +37,7 @@ with get_session() as db_session:
         for name, df in submission.dataFile.extract_sheets().items():
             file_name = '_'.join(name.lower().split()) + '.csv'
             df.to_csv(base_dir / file_name, index=False)
+
+        # Record a changelog entry
+        with open(base_dir / 'changes.log', 'a') as f:
+            print(f"[{timestamp.isoformat()}] Full export", file=f)
