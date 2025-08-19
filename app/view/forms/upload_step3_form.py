1df7273e55da2dc51aa4b532002369ff28cb1f4a
Andrew Radev <andrey.radev@gmail.com>
2025-08-19 13:30:41 +0200

    Validate uniqueness of techniques

--- app/view/forms/upload_step3_form.py
+++ app/view/forms/upload_step3_form.py
@@ -8,6 +8,7 @@ from wtforms import (
 from wtforms.validators import DataRequired
 
 from app.view.forms.base_form import BaseForm
+from app.model.orm import MeasurementTechnique
 
 
 class UploadStep3Form(BaseForm):
@@ -24,3 +25,12 @@ class UploadStep3Form(BaseForm):
         metaboliteIds = SelectMultipleField('metaboliteIds', choices=[], validate_choice=False)
 
     techniques = FieldList(FormField(TechniqueForm))
+
+    def validate_techniques(self, field):
+        techniques = [
+            MeasurementTechnique(type=t['type'], subjectType=t['subjectType'])
+            for t in field.data
+        ]
+        technique_descriptions = [mt.long_name_with_subject_type for mt in techniques]
+
+        self._validate_uniqueness("not unique", technique_descriptions)
