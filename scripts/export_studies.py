3cb98a4ed3429c3070bbdb3414df1ad99341298a
Andrew Radev <andrey.radev@gmail.com>
2025-08-19 12:15:05 +0200

    Export study on publish and on update

--- scripts/export_studies.py
+++ scripts/export_studies.py
@@ -1,9 +1,8 @@
-import os
 from pathlib import Path
 from datetime import datetime, UTC
 
-import simplejson as json
 import sqlalchemy as sql
+from long_task_printer import print_with_time
 
 from db import get_session
 from app.model.orm import Study
@@ -18,26 +17,6 @@ with get_session() as db_session:
     timestamp = datetime.now(UTC)
 
     for study in studies:
-        base_dir = Path(f"static/export/{study.publicId}")
-        base_dir.mkdir(parents=True, exist_ok=True)
-
-        submission = study.find_last_submission(db_session)
-
-        # Clean up previous files:
-        for file in base_dir.glob('*.csv'):
-            file.unlink()
-        for file in base_dir.glob('*.json'):
-            file.unlink()
-
-        # Export study design:
-        with open(base_dir / 'study_design.json', 'w') as f:
-            json.dump(submission.studyDesign, f, use_decimal=True, indent=2)
-
-        # Export data files:
-        for name, df in submission.dataFile.extract_sheets().items():
-            file_name = '_'.join(name.lower().split()) + '.csv'
-            df.to_csv(base_dir / file_name, index=False)
-
-        # Record a changelog entry
-        with open(base_dir / 'changes.log', 'a') as f:
-            print(f"[{timestamp.isoformat()}] Full export", file=f)
+        with print_with_time(f"> Exporting study {study.publicId}"):
+            submission = study.find_last_submission(db_session)
+            submission.export_data(message="Full export", timestamp=timestamp)
