0fcd94bb9c714a1ed2146ff8b4a9e7b35e131c95
Andrew Radev <andrey.radev@gmail.com>
2025-09-01 14:36:25 +0300

    Extract common js code

--- initialization/assets.py
+++ initialization/assets.py
@@ -21,6 +21,7 @@ def init_assets(app):
         '../app/view/js/lib/util.js',
         '../app/view/js/lib/page.js',
         '../app/view/js/lib/tooltips.js',
+        '../app/view/js/lib/compare_buttons.js',
         # Pages:
         '../app/view/js/upload/step1.js',
         '../app/view/js/upload/step2.js',
