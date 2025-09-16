52c9c39dd70a566b84f2eff4bf9c184bd11562c7
Andrew Radev <andrey.radev@gmail.com>
2025-09-16 11:52:11 +0200

    Use study/project publicId as a primary key

--- app/model/orm/study.py
+++ app/model/orm/study.py
@@ -24,12 +24,12 @@ class Study(OrmBase):
         cascade='all, delete-orphan',
     )
 
-    uuid:      Mapped[str] = mapped_column(sql.String(100), primary_key=True)
-    ownerUuid: Mapped[str] = mapped_column(sql.ForeignKey('Users.uuid'))
+    publicId: Mapped[str] = mapped_column(sql.String(100), primary_key=True)
+    uuid:     Mapped[str] = mapped_column(sql.String(100), nullable=False)
 
-    owner: Mapped['User'] = relationship(back_populates='ownedStudies')
+    ownerUuid: Mapped[str]    = mapped_column(sql.ForeignKey('Users.uuid'))
+    owner:     Mapped['User'] = relationship(back_populates='ownedStudies')
 
-    publicId:    Mapped[str] = mapped_column(sql.String(100))
     name:        Mapped[str] = mapped_column(sql.String(255))
     description: Mapped[str] = mapped_column(sql.String, nullable=True)
     url:         Mapped[str] = mapped_column(sql.String, nullable=True)
--- app/pages/projects.py
+++ app/pages/projects.py
@@ -8,10 +8,6 @@ from app.model.orm import Project
 
 
 def project_show_page(publicId):
-    project = g.db_session.scalars(
-        sql.select(Project)
-        .where(Project.publicId == publicId)
-        .limit(1)
-    ).one()
+    project = g.db_session.get_one(Project, publicId)
 
     return render_template("pages/projects/show.html", project=project)
