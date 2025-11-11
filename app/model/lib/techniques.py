b4451de6bba8fbcf773b94f9a5b6180bfa54da53
Andrew Radev <andrey.radev@gmail.com>
2025-11-11 19:59:51 +0100

    Fixes and reorganizations for study techniques

---
+++ app/model/lib/techniques.py
@@ -0,0 +1,21 @@
+TECHNIQUE_SHORT_NAMES = {
+    'fc':         'FC',
+    'od':         'OD',
+    'plates':     'PC',
+    '16s':        '16S-rRNA reads',
+    'qpcr':       'qPCR',
+    'ph':         'pH',
+    'metabolite': 'Metabolite',
+}
+"Human-readable short names of techniques"
+
+TECHNIQUE_LONG_NAMES = {
+    'fc':         'Flow Cytometry',
+    'od':         'Optical Density',
+    'plates':     'Plate Counts',
+    '16s':        '16S-rRNA reads',
+    'qpcr':       'qPCR',
+    'ph':         'pH',
+    'metabolite': 'Metabolites',
+}
+"Human-readable long names of techniques"
