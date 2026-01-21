8be7691b5dfaffdf3160767e9ba50b56dd1cd234
Andrew Radev <andrey.radev@gmail.com>
2026-01-21 16:34:23 +0100

    Implement "log view" buttons

--- app/view/js/sandbox.js
+++ app/view/js/sandbox.js
@@ -7,10 +7,13 @@ Page('.sandbox-page', function($page) {
 
   let $submitButton = $page.find('input[type=submit]');
 
+  $page.on('change', 'input[type=file]', updateSubmitState)
   $page.on('reset', 'form', function() {
     setTimeout(updateSubmitState, 1);
   });
-  $page.on('change', 'input[type=file]', updateSubmitState)
+
+  updateLogView();
+  $page.on('change', '.js-log-left,.js-log-right', updateLogView);
 
   function updateSubmitState() {
     if ($leftFileInput[0].files.length + $rightFileInput[0].files.length > 0) {
@@ -18,5 +21,28 @@ Page('.sandbox-page', function($page) {
     } else {
       $submitButton.prop('disabled', true);
     }
+  }
+
+  function updateLogView() {
+    let leftType;
+    if ($page.find('.js-log-left').is(':checked')) {
+      leftType = 'log';
+    } else {
+      leftType = 'linear';
+    }
+
+    let rightType;
+    if ($page.find('.js-log-right').is(':checked')) {
+      rightType = 'log';
+    } else {
+      rightType = 'linear';
+    }
+
+    $page.find('.js-plotly-plot').each(function() {
+      Plotly.relayout(this, {
+        'yaxis.type': leftType,
+        'yaxis2.type': rightType,
+      });
+    });
   }
 })
